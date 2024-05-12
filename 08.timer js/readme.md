a81508eafd077c90598de6bcbcb70683dbbf5fbd
arifur rahman <arifurrex@gmail.com>
2024-05-12 23:52:50 +0600

    javascript timmer

---
+++ 08.timer js/readme.md
@@ -0,0 +1,32 @@
+## Date()  
+this will return a string of date 'Tue May 07 2024 20:32:27 GMT+0600 (Bangladesh Standard Time)'
+
+## new Date()
+Tue May 07 2024 20:32:12 GMT+0600 (Bangladesh Standard Time)
+
+*Date()  এবং new Date() এর মদ্যে diffrent হচ্ছে  Date() string return করে আর new Date() string রিটার্ন করে না*
+
+#### new Date().getTime()
+এটা milisecond return করবে 1715092738338
+
+## new Date().getTime()/1000
+1000 দিয়ে ভাগ করলে milisecond থেকে second এ কনভার্ট হবে 
+
+## date.setHours(currentHours + futureHours);
+ if it's currently 10:30:00 and the user inputs 2 hours, 10 minutes, and 20 seconds, the resulting time would be 12:40:20.
+
+ *date.setHours(currentHours + futureHours);
+date.setMinutes(currentMinutes + futureMinutes);
+date.setSeconds(currentSeconds + futureSeconds);*
+
+explain it more :
+
+1.setHours: This function sets the hour component of a Date object to a specific value. For example, if you have a Date object representing 3:45:12 PM and you use setHours(8), it will set the time to 8:45:12 PM.
+
+2.setMinutes: Similar to setHours, setMinutes sets the minute component of a Date object to a specific value. For example, if you have a Date object representing 3:45:12 PM and you use setMinutes(30), it will set the time to 3:30:12 PM.
+
+3.setSeconds: Again, similar to the previous two, setSeconds sets the second component of a Date object to a specific value. For example, if you have a Date object representing 3:45:12 PM and you use setSeconds(20), it will set the time to 3:45:20 PM.
+
+
+#### new Date().getTime()
+এটা milisecond return করবে
---
+++ 08.timer js/script.js
@@ -0,0 +1,50 @@
+const hoursInput = document.getElementById('hoursInput');
+const minutesInput = document.getElementById('minutesInput');
+const secondsInput = document.getElementById('secondsInput');
+
+const button = document.getElementById('startTimer');
+
+const displayHours = document.getElementById('hoursOutput');
+const displayMinutes = document.getElementById('minutesOutput');
+const displaySecond = document.getElementById('secondsOutput');
+
+button.addEventListener('click',function(){
+  const futureHours =parseInt(hoursInput.value);  
+  const futureMinutes =parseInt(minutesInput.value);
+  const futureSeconds =parseInt(secondsInput.value);
+  
+
+  const date = new Date();
+  const currentHours = date.getHours();
+  const currentMinutes = date.getMinutes();
+  const currentSeconds = date.getSeconds();
+
+  date.setHours(currentHours + futureHours);
+  date.setMinutes(currentMinutes + futureMinutes);
+  date.setSeconds(currentSeconds + futureSeconds);
+
+  targetTime = date.getTime();
+  localStorage.setItem('targetTime', targetTime);
+  updateTimer();
+  timerInterval = setInterval(updateTimer, 500);
+    
+})
+
+
+const updateTimer = ()=>{
+  if(targetTime){
+  const differenceInSeconds = Math.floor(targetTime - Date.now()) / 1000; // convert milisecond to second devide by 1000
+
+  if (differenceInSeconds < 1) {
+    clearInterval(timerInterval);
+}
+
+        const hours = Math.floor(differenceInSeconds / 3600);
+        const minutes = Math.floor(differenceInSeconds / 60) % 60;
+        const seconds = Math.floor(differenceInSeconds % 60);
+
+        displayHours.textContent = `${hours} hours`;
+        displayMinutes.textContent = `${minutes} minutes`;
+        displaySecond.textContent = `${seconds} seconds`;
+  }
+}
